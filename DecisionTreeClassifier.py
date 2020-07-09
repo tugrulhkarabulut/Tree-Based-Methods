d31cba1985459420448040a63236b6371ec713da
= <=>
2020-07-09 03:30:53 +0300

    algorithm optimized. numpy arrays are used instead of dataframes

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import scipy.stats as st
 from time import time
 from sklearn import tree
 
@@ -11,39 +12,42 @@ class Node:
         self.split = split
         self.entropy = entropy
         self.children = []
-        self.parent = None
         self.leaf = False
         self.label = None
         self.depth = 0
         
-    def get_elements(self, X, y=None):
+    def get_split_indices(self, X, intersect_with=None):
+        X_feature = X[:, self.feature]
+
         # Categorical feature
-        if isinstance(self.split, list):
+        if isinstance(self.split, np.ndarray):
             splitted_data = []
             for value in self.split:
-                indices = X.iloc[:, self.feature] == value
-                if y is None:
-                    splitted_data.append(X[indices])
-                else:
-                    splitted_data.append((X[indices], y[indices]))
+                indices = np.asarray(X_feature == value).nonzero()[0]
+                if intersect_with is not None:
+                    indices = np.intersect1d(indices, intersect_with, assume_unique=True)
+                splitted_data.append(indices)
                     
             return splitted_data
         
         # Numerical feature
-        indices_left = X.iloc[:, self.feature] < self.split
-        indices_right = X.iloc[:, self.feature] >= self.split
-        
-        if y is None:
-            return [X[indices_left], X[indices_right]]
-        
-        return [(X[indices_left], y[indices_left]), (X[indices_right], y[indices_right])]
+        indices_left = np.asarray(X_feature < self.split).nonzero()[0]
+        indices_right = np.asarray(X_feature >= self.split).nonzero()[0]
+
+        if intersect_with is not None:
+            indices_left = np.intersect1d(indices_left, intersect_with, assume_unique=True)
+            indices_right = np.intersect1d(indices_right, intersect_with, assume_unique=True)
+
+        return [indices_left, indices_right]
             
     def entropy_for_split(self, X, y):
-        splitted_data = self.get_elements(X, y)
-        entropies = np.zeros(len(splitted_data))
-        for index, data in enumerate(splitted_data):
-            entropies[index] = self.calc_entropy(data[1]) * len(data[0])
-        return np.sum(entropies) / len(X)
+        splitted_indices = self.get_split_indices(X)  
+        entropies = np.zeros(len(splitted_indices))
+        len_X = len(X)
+        for index, branch_indices in enumerate(splitted_indices):
+            y_branch = y[branch_indices]
+            entropies[index] = self.calc_entropy(y_branch) * len(y_branch)
+        return np.sum(entropies) / len_X
     
     def calc_entropy(self, y, store=False):
         unique_y = np.unique(y)
@@ -68,14 +72,22 @@ class DecisionTreeClassifier:
     
     def fit(self, X, y):
         self.tree_ = Node()
-        self.__generate_tree(self.tree_, X, y)
+        X_ = self.__get_values(X)
+        y_ = self.__get_values(y)
+        #feature_types = [self.__check_type(X_[:, column]) for column in range(X.shape[1])]
+        feature_types = []
+        for column in range(X.shape[1]):
+            f_type = self.__check_type(X_[:, column])
+            print(f_type)
+            feature_types.append(f_type)
+        self.__generate_tree(self.tree_, X_, y_, feature_types)
     
-    def __generate_tree(self, tree, X, y):
+    def __generate_tree(self, tree, X, y, feature_types):
         if len(y) <= self.min_members or tree.calc_entropy(y, store=True) < self.tol:
             self.__label_node(tree, y)
             return
         
-        best_feature_split = self.__split_attribute(tree, X, y)        
+        best_feature_split = self.__split_attribute(tree, X, y, feature_types)        
         tree.feature = best_feature_split[0]
         tree.split = best_feature_split[1]
         
@@ -83,40 +95,41 @@ class DecisionTreeClassifier:
             self.__label_node(tree, y)
             return
         
-        splitted_data = tree.get_elements(X, y)
+        splitted_data = tree.get_split_indices(X)
         
         if len(splitted_data) < 2:
             self.__label_node(tree, y)
             return
                 
-        for el in splitted_data:
+        for branch_indices in splitted_data:
             new_node = Node()
             tree.children.append(new_node)
-            new_node.parent = tree
-            self.__generate_tree(new_node, el[0], el[1])
+            self.__generate_tree(new_node, X[branch_indices], y[branch_indices], feature_types)
         
     
-    def __split_attribute(self, tree, X, y):
-        min_entropy = 100 if tree.parent is None else tree.parent.entropy
+    def __split_attribute(self, tree, X, y, feature_types):
+        min_entropy = np.inf
         entropy = min_entropy
         best_feature = None
         best_split_value = None
-        for index, feature in enumerate(X.columns):
-            tree.feature = index
-            if X[feature].dtype.name == 'category':
-                tree.split = list(X[feature].unique())
+        for feature in range(X.shape[1]):
+            tree.feature = feature
+            X_feature = X[:, feature]
+            dtype = feature_types[feature]
+            if dtype == 'cat':
+                tree.split = np.unique(X_feature)
                 if len(tree.split) < 2:
                     continue
                 entropy = tree.entropy_for_split(X, y)
                 if entropy < min_entropy:
                     min_entropy = entropy
-                    best_feature = index
+                    best_feature = feature
                     best_split_value = tree.split
             else:
-                X_feature_sorted = X.iloc[1:, index].sort_values()
-                y_sorted = y[X_feature_sorted.index].values
-                X_feature_sorted_values = X_feature_sorted.values
-                thresholds = (X_feature_sorted_values[1:] + X_feature_sorted_values[:-1])/2
+                X_feature_sorted_indices = np.argsort(X_feature)
+                X_feature_sorted = X_feature[X_feature_sorted_indices]
+                y_sorted = y[X_feature_sorted_indices]
+                thresholds = (X_feature_sorted[1:] + X_feature_sorted[:-1])/2
                 thresholds_len = len(thresholds)
                 for value_index, value in enumerate(thresholds):
                     if (value_index < thresholds_len - 1) and (y_sorted[value_index] == y_sorted[value_index+1] or thresholds[value_index] == thresholds[value_index+1]):
@@ -127,29 +140,46 @@ class DecisionTreeClassifier:
 
                     if entropy < min_entropy:
                         min_entropy = entropy
-                        best_feature = index
+                        best_feature = feature
                         best_split_value = tree.split
                 
         return best_feature, best_split_value
     
     def __label_node(self, node, y):
-        most_frequent = y.mode()
+        most_frequent = st.mode(y)[0]
         rand = np.random.randint(len(most_frequent))
         node.leaf = True
-        node.label = y.mode()[rand]
-    
+        node.label = most_frequent[rand]
+
+    def __check_type(self, data):
+        try:
+            number_data = data.astype(np.number)
+            if np.all(np.mod(number_data, 1) == 0):
+                return 'cat' if len(np.unique(data)) / len(data) <= 0.05 else 'num'
+            return 'num'
+        except ValueError:
+            return 'cat'
+        
+    def __get_values(self, data):
+        if isinstance(data, np.ndarray):
+            return data
+        
+        return data.values
+
     def predict(self, X):
-        pred = pd.Series(-1, X.index)
-        self.__decide(self.tree_, X, pred)
+        X_ = self.__get_values(X)
+        pred = np.full(X_.shape[0], -1)
+        self.__decide(self.tree_, X_, pred, np.arange(X_.shape[0]))
         return pred
-    def __decide(self, node, X, pred):
+
+    def __decide(self, node, X, pred, indices):
         if node.leaf:
-            pred[X.index] = node.label
+            pred[indices] = node.label
             return
             
-        branches = node.get_elements(X)
+        branches = node.get_split_indices(X, indices)
         for index, branch in enumerate(branches):
-            self.__decide(node.children[index], branch, pred)
+            self.__decide(node.children[index], X, pred, branch)
     
     def score(self, X, y):
         y_pred = self.predict(X)

91385f9ea36f9cd636ae18ee40c6f3eb16c76a5d
= <=>
2020-07-11 23:06:46 +0300

    gini impurity measure is implemented. additional hyperparameters included. random forest implemented

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import scipy.stats as st
-from time import time
 from collections import deque
 
 class Node:
@@ -15,6 +14,11 @@ class Node:
         self.leaf = False
         self.label = None
         self.depth = 0
+    
+    def get_depth(self):
+        if self.leaf or len(self.children) == 0 :
+            return 1
+        return 1 + max([child.get_depth() for child in self.children])
         
     def get_split_indices(self, X, intersect_with=None):
         X_feature = X[:, self.feature]
@@ -85,26 +89,44 @@ class Node:
 
 
 class DecisionTreeClassifier:
-    def __init__(self, tol=0.5, max_depth=10, min_members=50, criterion='entropy'):
+    def __init__(self, 
+                 tol=None, 
+                 max_depth=None, 
+                 min_members=10, 
+                 criterion='entropy', 
+                 split_method='nary', 
+                 max_features=None):
         self.tol = tol
         self.tree_depth = 0
         self.max_depth = max_depth
         self.min_members = min_members
         self.criterion = criterion
+        self.split_method = split_method
+        self.max_features = max_features
     
     def fit(self, X, y):
         self.tree_ = Node()
-        self.features_ = X.columns if isinstance(X, pd.DataFrame) else list(range(X.shape[1]))
-        self.important_features_ = []
         X_ = self.__get_values(X)
         y_ = self.__get_values(y)
-        feature_types = [self.__check_type(X_[:, column]) for column in range(X.shape[1])]
+        if self.split_method == 'binary':
+            feature_types = None
+        elif self.split_method == 'nary':
+            feature_types = [self.__check_type(X_[:, column]) for column in range(X.shape[1])]
+        else:
+            raise ValueError('parameter feature_types must be binary or nary')
         self.__generate_tree(self.tree_, X_, y_, feature_types)
     
     def __generate_tree(self, tree, X, y, feature_types):
-        if len(y) <= self.min_members or tree.calc_impurity(y, store=True, criterion=self.criterion) < self.tol:
+        if len(y) <= self.min_members:
             self.__label_node(tree, y)
             return
+
+        if self.tol and tree.calc_impurity(y, store=True, criterion=self.criterion) < self.tol:
+            self.__label_node(tree, y)
+            return
+        
+        if self.max_features and self.tree_.get_depth() >= self.max_depth:
+            self.__label_node(tree, y)
         
         best_feature_split = self.__split_attribute(tree, X, y, feature_types)        
         tree.feature = best_feature_split[0]
@@ -114,8 +136,6 @@ class DecisionTreeClassifier:
             self.__label_node(tree, y)
             return
         
-        self.important_features_.append(self.features_[tree.feature])
-
         splitted_data = tree.get_split_indices(X)
         num_branches = len(splitted_data)
         if num_branches < 2:
@@ -129,25 +149,33 @@ class DecisionTreeClassifier:
         for branch_indices in splitted_data:
             new_node = Node()
             tree.children.append(new_node)
-            self.__generate_tree(new_node, X[branch_indices], y[branch_indices], feature_types)
+            self.__generate_tree(new_node, X[branch_indices], y[branch_indices], feature_types=None)
         
     
-    def __split_attribute(self, tree, X, y, feature_types):
-        min_entropy = np.inf
-        entropy = min_entropy
+    def __split_attribute(self, tree, X, y, feature_types=None):
+        min_impurity = np.inf
+        impurity = min_impurity
         best_feature = None
         best_split_value = None
-        for feature in range(X.shape[1]):
+
+        features = []
+        if not self.max_features:
+            features = np.arange(X.shape[1])    
+        elif self.max_features == 'auto':
+            features = np.random.choice(X.shape[1], size=np.sqrt(X.shape[1]).astype('int'), replace=False)
+        else:
+            features = np.random.choice(X.shape[1], size=self.max_features, replace=False)
+
+        for feature in features:
             tree.feature = feature
             X_feature = X[:, feature]
-            dtype = feature_types[feature]
-            if dtype == 'cat':
+            if feature_types is not None and feature_types[feature] == 'cat':
                 tree.split = np.unique(X_feature)
                 if len(tree.split) < 2:
                     continue
-                entropy = tree.impurity_for_split(X, y, criterion=self.criterion)
-                if entropy < min_entropy:
-                    min_entropy = entropy
+                impurity = tree.impurity_for_split(X, y, criterion=self.criterion)
+                if impurity < min_impurity:
+                    min_impurity = impurity
                     best_feature = feature
                     best_split_value = tree.split
             else:
@@ -161,14 +189,14 @@ class DecisionTreeClassifier:
                         continue
                     
                     tree.split = value
-                    entropy = tree.impurity_for_split(X, y, criterion=self.criterion)
+                    impurity = tree.impurity_for_split(X, y, criterion=self.criterion)
 
-                    if entropy < min_entropy:
-                        min_entropy = entropy
+                    if impurity < min_impurity:
+                        min_impurity = impurity
                         best_feature = feature
                         best_split_value = tree.split
                 
-        return best_feature, best_split_value
+        return best_feature, best_split_value, min_impurity
     
     def __label_node(self, node, y):
         most_frequent = st.mode(y)[0]
@@ -186,10 +214,9 @@ class DecisionTreeClassifier:
             return 'cat'
         
     def __get_values(self, data):
-        if isinstance(data, np.ndarray):
-            return data
-        
-        return data.values
+        if isinstance(data, pd.DataFrame) or isinstance(data, pd.Series):
+            return data.values
+        return data
 
     def predict(self, X):
         X_ = self.__get_values(X)
@@ -209,3 +236,9 @@ class DecisionTreeClassifier:
     def score(self, X, y):
         y_pred = self.predict(X)
         return y_pred[y == y_pred].size / y_pred.size
+    
+    def __str__(self):
+        return "DecisionTreeClassifier(tol={}, max_depth={}, min_members={}, criterion={}, split_method={}, max_features={})".format(self.tol, self.max_depth, self.min_members, self.criterion, self.criterion, self.split_method)
+    
+    def __repr__(self):
+        return "DecisionTreeClassifier(tol={}, max_depth={}, min_members={}, criterion={}, split_method={}, max_features={})".format(self.tol, self.max_depth, self.min_members, self.criterion, self.criterion, self.split_method)
